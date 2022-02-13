cb59cccff0f890b00258e64893036048b4addca9
AhmedRaza1211 <ahmedraza8325@gmail.com>
2022-02-13 18:14:58 +0500

    button.js code completed

---
+++ src/component/Button/button.js
@@ -0,0 +1,10 @@
+const Button = ({ onClickSending }) => {
+
+    return (
+        <div>
+            <button onClick={onClickSending} > Add Todo </button>
+        </div>
+    )
+}
+
+export default Button;
