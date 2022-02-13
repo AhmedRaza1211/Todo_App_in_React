faccaf11fb93c66dd89f6d4b1722a254a308ebd7
AhmedRaza1211 <ahmedraza8325@gmail.com>
2022-02-13 18:12:08 +0500

    todo.js completed

--- src/component/Todo/todo.js
+++ src/component/Todo/todo.js
@@ -1,10 +1,24 @@
+// TODOS
 import react, { useState } from 'react';
+import Input from '../Input/input';
+import Button from '../Button/button';
+import List from '../List/list';
 
 const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const onInputChange = (e) => setInputValue(e.target.value);
+    const addTodo = () => {
+        setTodos([...todos, inputValue]);
+        setInputValue('');
+    }
     
     return (
         <div>
             <h1> Todo App</h1>
+            <Input val={inputValue} onInputChangeSending={onInputChange} />
+            <Button onClickSending={addTodo} />
+            <List showTodoSending={todos}/>
         </div>
     )
 }
