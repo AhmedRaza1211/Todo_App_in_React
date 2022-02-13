318a45b5dbd96d168f471d5e3847edabfdf0124f
AhmedRaza1211 <ahmedraza8325@gmail.com>
2022-02-13 18:02:24 +0500

    making only components

---
+++ src/component/Todo/todo.js
@@ -0,0 +1,12 @@
+import react, { useState } from 'react';
+
+const Todo = () => {
+    
+    return (
+        <div>
+            <h1> Todo App</h1>
+        </div>
+    )
+}
+
+export default Todo;
